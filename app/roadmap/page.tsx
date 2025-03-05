8d9e5b1e39e7210d89f15d57a44c13cbef73f0c3
nikeshcohen <nikeshcohen@outlook.com>
2025-03-05 16:11:28 +0200

    add roadmap

---
+++ app/roadmap/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+import Roadmap from "./Roadmap";
+
+export const metadata: Metadata = {
+  title: "RoadMap | ProfilePrep",
+  description: "Track the development progress of ProfilePrep.",
+};
+
+export default function RoadmapPage() {
+  return (
+    <section className="layout flex min-h-[93vh] items-center justify-center">
+      <Roadmap />
+    </section>
+  );
+}
---
+++ constants/roadmap.ts
@@ -0,0 +1,27 @@
+export const roadmap = [
+  {
+    id: 1,
+    date: "Feb 13, 2025",
+    title: "Version 0 Launch",
+    description: "Initial release of the application with core features.",
+  },
+  {
+    id: 2,
+    date: "Feb 25, 2025",
+    title: "Authentication System",
+    description: "Implementation of user and company authentication.",
+  },
+  {
+    id: 3,
+    date: "March 1, 2025",
+    title: "User Management",
+    description: "Enable admins to manage users within their company.",
+  },
+  {
+    id: 4,
+    date: "March 2, 2025",
+    title: "API & Security Enhancements",
+    description:
+      "Strengthening security measures and optimizing API performance.",
+  },
+];
