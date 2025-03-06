7a1a9f8e492b0ea627722616e19eeae7a70b6113
nikeshcohen <nikeshcohen@outlook.com>
2025-03-06 12:13:05 +0200

    add auth cache

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import InfoButton from "@/components/global/InfoButton";
@@ -12,9 +13,11 @@ function Footer() {
   return (
     <footer className="ml-auto mr-auto flex max-w-[1800px] flex-col items-center justify-center space-y-4 border-t p-4 lg:flex-row lg:justify-between lg:space-y-0">
       <div className="flex h-14 w-14 items-center justify-center rounded-full bg-primary/20">
-        <h1 className="text-xl font-bold tracking-wider">
-          Profile<span className="text-primary">Prep</span>
-        </h1>
+        <Link href="/">
+          <h1 className="text-xl font-bold tracking-wider">
+            Profile<span className="text-primary">Prep</span>
+          </h1>
+        </Link>
       </div>
 
       <div className="flex gap-2">
