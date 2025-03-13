1fbd790471a50f7bbda16a1e8d505b344af2c7e5
Jediah Jireh Naicker <162832021+jediahjireh@users.noreply.github.com>
2025-03-13 12:46:42 +0200

    fix: wrap `useSearchParams` in a suspense boundary to resolve csr bailout error

--- app/app/onboarding/page.tsx
+++ app/app/onboarding/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import { Metadata } from "next";
 
 import { OnboardingClient } from "@/app/app/onboarding/_components/OnboardingClient";
@@ -9,18 +11,25 @@ export const metadata: Metadata = {
 export default function OnboardingPage() {
   return (
     <section className="layout flex min-h-[93vh] place-content-center place-items-center">
-      <div>
-        <h1 className="mb-6 space-y-1 text-center text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-          Welcome to Profile
-          <span className="font-bold text-primary">Prep</span>
-        </h1>
-
-        <p className="mx-auto mb-4 max-w-[700px] pb-2.5 text-center text-muted-foreground">
-          Let&apos;s get you onboarded!
-        </p>
-      </div>
-
-      <OnboardingClient />
+      <Suspense fallback={<OnboardingBackground />}>
+        <OnboardingBackground />
+        <OnboardingClient />
+      </Suspense>
     </section>
+  );
+}
+
+function OnboardingBackground() {
+  return (
+    <div>
+      <h1 className="mb-6 space-y-1 text-center text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+        Welcome to Profile
+        <span className="font-bold text-primary">Prep</span>
+      </h1>
+
+      <p className="mx-auto mb-4 max-w-[700px] pb-2.5 text-center text-muted-foreground">
+        Let&apos;s get you onboarded!
+      </p>
+    </div>
   );
 }
